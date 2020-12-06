ebda96f0013bf19d06051adfc6cb344b49b1da93
dev <dev@example.com>
2020-12-06 22:17:06 +0800

    4-3 用户注册-后端代码实现-1

--- housev1.0/house-web/src/main/java/com/mooc/house/web/controller/UserController.java
+++ housev1.0/house-web/src/main/java/com/mooc/house/web/controller/UserController.java
@@ -2,8 +2,12 @@ package com.mooc.house.web.controller;
 
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import com.mooc.house.biz.service.UserService;
+import com.mooc.house.common.result.ResultMsg;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +18,34 @@ public class UserController {
 
   @Autowired
   private UserService userService;
+
+  @Autowired
+  private UserService userService;
   
   @RequestMapping("user")
   public List<User> getUsers(){
     return userService.getUsers();
   }
-  
 
+  /**
+   * 注册提交:1.注册验证 2 发送邮件 3验证失败重定向到注册页面 注册页获取:根据account对象为依据判断是否注册页获取请求
+   *
+   * @param account
+   * @param modelMap
+   * @return
+   */
+  @RequestMapping("accounts/register")
+  public String accountsRegister(User account, ModelMap modelMap) {
+    if (account == null || account.getName() == null) {
+      return "/user/accounts/register";
+    }
+    // 用户验证
+    ResultMsg resultMsg = UserHelper.validate(account);
+    if (resultMsg.isSuccess() && userService.addAccount(account)) {
+      modelMap.put("email", account.getEmail());
+      return "/user/accounts/registerSubmit";
+    } else {
+      return "redirect:/accounts/register?" + resultMsg.asUrlParams();
+    }
+  }
 }
